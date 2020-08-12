20b1da4204b318938b842f0b53a96f649e688c64
AlexanderBotnari <63365662+AlexanderBotnari@users.noreply.github.com>
2020-08-12 19:42:55 +0300

    Update Money.java

--- src/domain/Money.java
+++ src/domain/Money.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
-public class Money {
+public class Money implements Serializable{
 
 	private static CurrencyProvider currencyProvider = new CurrencyProvider();
 	
